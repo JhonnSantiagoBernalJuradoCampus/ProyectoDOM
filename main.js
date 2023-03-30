c5af31d9a117d6ba25a64e1e20993b45c0b9c3f9
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-29 22:13:11 -0500

    feat: :sparkles: Main js

--- main.js
+++ main.js
@@ -3,10 +3,7 @@ import myBanner from "./components/myBanner.js";
 import myCards from "./components/myCards.js";
 import myAside from "./components/myAside.js";
 import myFooter from "./components/myFooter.js";
-/* import mySection1 from "./components/mySection1.js";
-import mySection2 from "./components/mySection2.js";
-import mySection3 from "./components/mySection3.js";
-import myFooter from "./components/myFooter.js"; */
+import myMain from "./components/myMain.js";
 
 myHeader.fragShow();
 
@@ -19,13 +16,8 @@ myCards.fragShow();
 myAside.fragShow();
 
 myFooter.fragShow();
-/* mySection1.showArticle();
-mySection1.showList3();
-mySection1.showEnd();
 
-mySection2.showFirstPart();
-mySection2.showTable(); */
-/* 
-mySection3.showSection();
-mySection3.showNav();
-*/
+myMain.listSection1();
+myMain.listSection2();
+myMain.listTable();
+myMain.listSection3();
